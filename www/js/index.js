20fb1ae0cc18c9711dbbaa701f2a3f438b632d4a
clientelis <alex@clientelis.com>
2025-01-11 18:40:08 +0000

    version one line through when swiped right

---
+++ www/js/index.js
@@ -0,0 +1,71 @@
+window.onload = function (){
+    const addButton = document.querySelector('input[value=Ajouter]');
+    const resetButton = document.querySelector('input[value=Réinitialiser');
+    
+    const taskfield = document.getElementById('task');
+    const tasklist= document.getElementById('taskList');
+    const finishedTasks = document.getElementById('done');
+
+
+
+    addButton.onclick = function (){
+        const item = document.createElement('li');
+        
+        item.innerHTML = taskfield.value;
+
+        tasklist.append(item);
+        $(item).on('swiperight',function (){
+            $(item).toggleClass('terminer')
+        })
+
+        $(item).on('swipeleft',function (){
+            $(this).hide('slow',function(){
+                $(this).remove();
+            });
+        })
+        
+        $(tasklist).listview('refresh');
+        taskfield.select();
+    };
+    
+    
+    //     addButton.onclick = function (){
+    //         const item = document.createElement('li');
+    //         item.innerHTML = taskfield.value;
+
+    //         tasklist.innerHTML += `<li>${taskfield.value}</li>`;
+    //                 taskfield.select();
+    //                 $(tasklist).listview('refresh');
+                
+    //         };
+    //     };
+        
+    //     $(item).on('swiperight', function(){
+    //         console.log('swiped right');
+    //         $(item).toggleClass('terminer');
+
+
+    //     })
+
+
+    //     $(item).on('swipeleft', function(){
+
+    //         $(item).hide('slow', function(){;
+    //             $(item).remove();
+    //     })
+    // })
+    
+    //     resetButton.onclick = function () {
+    //         taskList.innerHTML = ''; 
+    //         $(taskList).listview('refresh'); 
+    //     };
+    
+    };
+
+    //v1 branche 1
+    //swiple right line through
+    //swipe left delete
+
+    //v2 branche 2
+    //swipe left pour supprimer
+    //swipe right pour mettre dans le compartiment corresponant soit en cours ou terminer
